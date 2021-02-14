104944d41499f0f4f893ddb3c1fea535cb7f6547
hol@svep.se <hol@svep.se>
2021-02-14 15:05:03 +0100

    Saves to PNG successfully.

--- bin/spritesheet/spritesheet.go
+++ bin/spritesheet/spritesheet.go
@@ -3,13 +3,16 @@ package spritesheet
 import (
 	"fmt"
 	"image"
+	"image/png"
 	"os"
 
 	"github.com/holmqvist1990/go-spritepack/bin/sprite"
 )
 
 type Spritesheet struct {
-	Sprites sprite.Sprites
+	Sprites  sprite.Sprites
+	bounds   image.Rectangle
+	tileSize int
 }
 
 func FromFile(file *os.File, tileSize int) (*Spritesheet, error) {
@@ -18,11 +21,15 @@ func FromFile(file *os.File, tileSize int) (*Spritesheet, error) {
 		return nil, fmt.Errorf("tileset.FromFile: %w", err)
 	}
 	return &Spritesheet{
-		Sprites: sprite.NewSpritesFromImage(img, tileSize),
+		Sprites:  sprite.NewSpritesFromImage(img, tileSize),
+		bounds:   img.Bounds(),
+		tileSize: tileSize,
 	}, nil
 }
 
 func (sp *Spritesheet) FilterUnique() {
+	sp.Sprites = sp.Sprites.ToSet()
+
 	spriteMap := make(map[string]sprite.Sprite)
 	for _, sprite := range sp.Sprites {
 		_, ok := spriteMap[sprite.Checksum()]
@@ -36,6 +43,39 @@ func (sp *Spritesheet) FilterUnique() {
 		newSprites = append(newSprites, v)
 	}
 
-	// TODO: Test! :)
 	sp.Sprites = newSprites
+}
+
+func (sp *Spritesheet) SaveToFile(filename string) error {
+	img := image.NewRGBA(sp.bounds)
+	sp.spritesToImage(img)
+	file, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	err = png.Encode(file, img)
+	if err != nil {
+		return nil
+	}
+	return nil
+}
+
+func (sp *Spritesheet) spritesToImage(img *image.RGBA) {
+	var x, y int
+	for _, sprite := range sp.Sprites {
+		sp.spriteToImage(img, sprite, x, y)
+		x += sp.tileSize
+		if x >= sp.bounds.Max.X {
+			x = 0
+			y += sp.tileSize
+		}
+	}
+}
+
+func (sp *Spritesheet) spriteToImage(img *image.RGBA, sprite sprite.Sprite, xOffset, yOffset int) {
+	for x, row := range sprite {
+		for y, col := range row {
+			img.Set(x+xOffset, y+yOffset, col)
+		}
+	}
 }
